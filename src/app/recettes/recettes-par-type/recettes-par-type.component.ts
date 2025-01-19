2d92b9af3bbb584a777cd65f6bf2b9e856dd1495
brice150 <brice.lecomte0@gmail.com>
2025-01-19 17:28:56 +0100

    search recipes works

--- src/app/recettes/recettes-par-type/recettes-par-type.component.ts
+++ src/app/recettes/recettes-par-type/recettes-par-type.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, Input, OnInit } from '@angular/core';
+import { Component, inject, Input, OnDestroy, OnInit } from '@angular/core';
 import { VideComponent } from './vide/vide.component';
 import { RecetteComponent } from './recette/recette.component';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
@@ -8,6 +8,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Recipe } from '../../core/interfaces/recipe';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-recettes-par-type',
@@ -24,11 +25,12 @@ import { Recipe } from '../../core/interfaces/recipe';
   templateUrl: './recettes-par-type.component.html',
   styleUrl: './recettes-par-type.component.css',
 })
-export class RecettesParTypeComponent implements OnInit {
+export class RecettesParTypeComponent implements OnInit, OnDestroy {
   searchForm!: FormGroup;
   fb = inject(FormBuilder);
   @Input() recipes: Recipe[] = [];
   filteredRecipes: Recipe[] = [];
+  destroyed$: Subject<void> = new Subject<void>();
 
   ngOnInit(): void {
     this.searchForm = this.fb.group({
@@ -36,5 +38,27 @@ export class RecettesParTypeComponent implements OnInit {
     });
 
     this.filteredRecipes = [...this.recipes];
+
+    this.searchForm
+      .get('search')
+      ?.valueChanges.pipe(takeUntil(this.destroyed$))
+      .subscribe((searchValue: string) => {
+        this.filteredRecipes = this.filterRecipes(searchValue);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
+
+  filterRecipes(searchValue: string): Recipe[] {
+    if (!searchValue) {
+      return [...this.recipes];
+    }
+
+    return this.recipes.filter((recipe) =>
+      recipe.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
   }
 }
