b1396a80ee788358bd4d0349281928fac6bdce8f
brice150 <brice.lecomte0@gmail.com>
2025-06-23 21:42:13 +0200

    translate

--- src/app/connect/welcome/welcome.component.ts
+++ src/app/connect/welcome/welcome.component.ts
@@ -1,5 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { environment } from '../../../environments/environment';
 
@@ -9,6 +15,28 @@ import { environment } from '../../../environments/environment';
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css',
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements AfterViewInit {
   imagePath: string = environment.imagePath;
+  @ViewChildren('feature') features!: QueryList<ElementRef>;
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry, index) => {
+          if (entry.isIntersecting) {
+            const element = entry.target as HTMLElement;
+            const relativeDelay = index * 0.2;
+            element.style.transitionDelay = `${relativeDelay}s`;
+            element.classList.add('visible');
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      { threshold: 0.2 }
+    );
+
+    this.features.forEach((feature) => {
+      observer.observe(feature.nativeElement);
+    });
+  }
 }
